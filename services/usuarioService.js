eb917b0ca274ff51c755cdf7386d3ee9870a279d
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-05-21 13:28:34 -0300

    refactor: replace bcrypt with bcryptjs in usuarioService

--- services/usuarioService.js
+++ services/usuarioService.js
@@ -1,5 +1,5 @@
 // Serviço de usuário 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const UsuarioModel = require('../models/usuarioModel');
 require('dotenv').config();
